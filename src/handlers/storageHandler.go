54397d41d254b2e50509e6c8f59b1ee2bf2eb77a
mlapeyre <lapeyre.martial@gmail.com>
2014-08-06 19:35:27 +0200

    StorageHandler: handling several test case

--- src/handlers/storageHandler.go
+++ src/handlers/storageHandler.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"path/filepath"
+	"os"
 )
 
 type StorageHandler struct {
@@ -23,12 +24,39 @@ func (handler *StorageHandler) CreateHandler() func(w http.ResponseWriter, r *ht
 	return func(w http.ResponseWriter, r *http.Request) {
 		realPath := resolveRealPath(handler, r.URL.Path)
 		fmt.Fprintf(w, "Trying to get %s", realPath)
+		fileInfo, err := os.Stat(realPath)
+		if (err != nil) {
+			if (os.IsNotExist(err)) {
+				fmt.Fprintf(w, "File %s not found", fileInfo.Name())
+				fmt.Fprintf(w, "File %s not found", err)
+			}
+		}else {
+			if(hasIndexFile(realPath)){
+				fmt.Fprintf(w, "RETURN THE INDEX FILE ! %s", fileInfo.Name())
+			}else{
+				if(fileInfo.IsDir()){
+					fmt.Fprintf(w, "LIST FILE CHILDREN %s", fileInfo.Name())
+				}else{
+					fmt.Fprintf(w, "Return FILE %s", fileInfo.Name())
+				}
+			}
+		}
+	}
+}
+
+
+func hasIndexFile(folderPath string) bool {
+	fileInfo , err := os.Stat(filepath.Join(folderPath, "index.html"))
+	if(err != nil){
+		//isNOtExistOrAnother problem lets assume that we won't be able to use it
+		return false;
+	}else{
+		return fileInfo.IsDir()==false
 	}
 }
 
 func resolveRealPath(handler *StorageHandler, requestedPath string) string {
-  	base := filepath.Clean(handler.UrlBase)
+	base := filepath.Clean(handler.UrlBase)
 	value := requestedPath[len(base):len(requestedPath)]
-	fmt.Println("value="+value)
-	return filepath.Join(handler.InternalPath,value)
+	return filepath.Join(handler.InternalPath, value)
 }
--- src/handlers/storageHandler_test.go
+++ src/handlers/storageHandler_test.go
@@ -2,34 +2,67 @@ package handlers
 
 import (
 	"testing"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 )
+
 var testData = []struct {
-		basePattern string
-		urlRequest string
-		internalPath string
-		resolvedPath string
+	basePattern  string
+	urlRequest   string
+	internalPath string
+	resolvedPath string
 }{
-	{"/browse/","/browse","/home/martial/","/home/martial"},
-	{"/browse/","/browse/","/home/martial/","/home/martial"},
-	{"/browse/","/browse/mama","/home/martial/","/home/martial/mama"},
-	{"/browse/","/browse/mama","/home/martial","/home/martial/mama"},
-	{"/browse/","/browse/mama/","/home/martial","/home/martial/mama"},
-	{"/browse/film","/browse/film/mama/","/home/martial","/home/martial/mama"},
+	{"/browse/", "/browse", "/home/martial/", "/home/martial"},
+	{"/browse/", "/browse/", "/home/martial/", "/home/martial"},
+	{"/browse/", "/browse/mama", "/home/martial/", "/home/martial/mama"},
+	{"/browse/", "/browse/mama", "/home/martial", "/home/martial/mama"},
+	{"/browse/", "/browse/mama/", "/home/martial", "/home/martial/mama"},
+	{"/browse/film", "/browse/film/mama/", "/home/martial", "/home/martial/mama"},
 }
 
+//========================================================================================
+//  Tests
+//========================================================================================
 func TestResolveRealPath(t *testing.T) {
-	for _,element := range testData {
+	for _, element := range testData {
 		storageHandler := New(element.basePattern, element.internalPath)
-		value := resolveRealPath(storageHandler,element.urlRequest)
-		assertEquals(value,element.resolvedPath,t)
+		value := resolveRealPath(storageHandler, element.urlRequest)
+		assertEquals(value, element.resolvedPath, t)
 	}
 }
 
-func assertEquals(first string, second string,t *testing.T){
-	if(first == second){
+func TestShouldHasIndexBeTrueWhenTheFolderContainsAnIndexFile(t *testing.T) {
+	tmpPath, _ := ioutil.TempDir(os.TempDir(), "go-test")
+	defer os.RemoveAll(tmpPath)
+	os.Create(filepath.Join(tmpPath, "index.html"))
+	assertTrue(hasIndexFile(tmpPath),t)
+}
+
+func TestShouldHasIndexBeFalseWhenTheFolderDoesntContainAnIndexFile(t *testing.T) {
+	tmpPath, _ := ioutil.TempDir(os.TempDir(), "go-test")
+	defer os.RemoveAll(tmpPath)
+	assertFalse(hasIndexFile(tmpPath),t)
+}
+
+
+//========================================================================================
+//  Utils
+//========================================================================================
+func assertTrue(predicate bool,t *testing.T) {
+	if(predicate!=true){
+		t.Fail()
+	}
+}
+func assertFalse(predicate bool,t *testing.T) {
+	assertTrue(!predicate,t)
+}
+
+func assertEquals(first string, second string, t *testing.T) {
+	if (first == second) {
 		return;
-	}else{
-		t.Errorf("%s != %s",first,second)
+	}else {
+		t.Errorf("%s != %s", first, second)
 		t.Fail()
 	}
 }
