353d71e95d8ba03e35ffeb476a3005ec616df94e
Martial <martial@linux-s1z5.site>
2014-08-07 22:45:33 +0200

    trying to use real good hierarchy convention...

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"net/http"
-	"github.com/mlapeyre/handlers"
+	"github.com/mlapeyre/prettyFileServe"
 	"log"
 )
 
 
 func main() {
-	var storageHandler * handlers.StorageHandlerDefinition = handlers.New("/browse/", "/tmp/martial")
+	var storageHandler * prettyFileServe.StorageHandlerDefinition = prettyFileServe.New("/browse/", "/tmp/martial")
 	http.HandleFunc(storageHandler.UrlBase, storageHandler.CreateHandler())
 	//TODO 404 handling
 	error := http.ListenAndServe(":8080", nil)
